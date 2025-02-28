28a959a513ee74321c9ea81e95ffb7927032e5d7
chan0985 <chandra.prakash@rackspace.com>
2025-02-28 23:17:35 +0530

    updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 //import logo from '../../assets/logo.png'
-import ats_logo from '../../assets/ats-logo.png'
+//import ats_logo from '../../assets/ats-logo.png'
 import menu_icon from '../../assets/menu-icon.png'
 import { Link } from 'react-scroll';
 
@@ -24,7 +24,7 @@ const Navbar = () => {
 
   return (
     <nav className={`container ${sticky? 'dark-nav' : ''}`}>
-      <img src={ats_logo} alt="" className='logo' />
+      <img src="" alt="" className='logo' />
       <ul className={mobileMenu?'':'hide-mobile-menu'}>
         <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
         <li><Link to='program' smooth={true} offset={-260} duration={500}>Services</Link></li>
